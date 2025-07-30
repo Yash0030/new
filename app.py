ee48707b614bbe6410204a15cad386617cae159a
Yash0030 <158812186+Yash0030@users.noreply.github.com>
2025-07-30 23:01:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def hackrx_run():
         # Process each question
         for q in questions:
             result = expand_query_and_thought(q)
-            expanded_query = result
+            expanded_query = result["expanded_query"]
     
 
             retrieved_docs = vectorstore.similarity_search(expanded_query, k=5)
