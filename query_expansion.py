a179126537e5bfe5465c8bd8a1cd9b15160f196e
Yash0030 <158812186+Yash0030@users.noreply.github.com>
2025-07-30 19:17:52 +0530

    Update query_expansion.py

--- query_expansion.py
+++ query_expansion.py
@@ -5,12 +5,15 @@ from langchain_core.runnables import Runnable
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Voxtral-Mini-3B-2507",
+    repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
     task="text-generation",
     huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
-    temperature=0.0
+    temperature=0.0,
+    top_k=1,
+    top_p=1.0,
+    do_sample=False,
+    repetition_penalty=1.0,
 )
 
 model=ChatHuggingFace(llm=llm)
@@ -21,11 +24,11 @@ parser = JsonOutputParser()
 # LangChain Prompt Template
 prompt = ChatPromptTemplate.from_messages([
     ("system", 
-     """You are an intelligent assistant that expands the query only when its vague insurance or policy-related queries 
+     """You are an intelligent assistant that expands vague insurance or policy-related queries 
      and provides a reasoning path for document retrieval and decision making.
      
      Given a natural language query, do two things:
-     1. Expand the query with relevant structure by extracting these information if available (age, location, treatment, policy duration).
+     1. Expand the query with relevant structure (age, location, treatment, policy duration).
      2. Generate a chain of reasoning steps that explain how a decision could be made using policy clauses.
      
      Return JSON with two fields: 
